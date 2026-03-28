fd3eb0eeb9b992b307982ad4bf2d7120de5a6e4e
Aditya Handa <handaaditya470@gmail.com>
2026-03-28 11:26:18 +0530

    third  commit

--- staioneryy/urls.py
+++ staioneryy/urls.py
@@ -18,11 +18,15 @@ Including another URLconf
 from django import views
 import django
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+     path("items/", include('staitionary_items.url')),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+   
 
 ]
 
--- staioneryy/views.py
+++ staioneryy/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse("<h1>Welcome to staionery management system by aditya </h1>")
+  return HttpResponse("<h1>Welcome to staionery management system by aditya </h1> <h2> This is printing and stationery services</h2> <h3> We provide all the services related to printing and stationery</h3>")
 
 def about(request):
   return HttpResponse("<h1>staionery mangement system</h1>")
